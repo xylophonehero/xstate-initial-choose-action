63f5c938eb6d836b1f059c128c79d005e41c3164
xylophonehero <nick97000@gmail.com>
2022-06-28 14:48:33 +0100

    feat: bad choose action on initial state

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,60 @@
 import "./styles.css";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { createMachine, assign } from "xstate";
+import { createMachine, assign , actions} from "xstate";
 import { useMachine } from "@xstate/react";
+const {choose}= actions
 
 interface ToggleContext {
   count: number;
 }
 
-const toggleMachine = createMachine<ToggleContext>({
+const toggleMachine = createMachine({
+  tsTypes: {} as import("./index.typegen").Typegen0,
+  schema: {
+    context: {} as ToggleContext,
+    events: { type: 'TOGGLE'}
+  },
   id: "toggle",
   initial: "inactive",
   context: {
     count: 0
   },
+  entry: choose([
+    {
+      cond: 'someCondition',
+      actions: 'someAction'
+    }
+  ]),
   states: {
     inactive: {
       on: { TOGGLE: "active" }
     },
     active: {
-      entry: assign({ count: (ctx) => ctx.count + 1 }),
+      entry: [
+        assign({ count: (ctx) => ctx.count + 1 }), 
+        // Uncomminting out the below lines removes the error
+        // choose([
+        //   {
+        //     cond: 'someCondition',
+        //     actions: 'someAction'
+        //   }
+        // ])
+      ],
       on: { TOGGLE: "inactive" }
     }
   }
 });
 
 function App() {
-  const [current, send] = useMachine(toggleMachine);
+  const [current, send] = useMachine(toggleMachine, {
+    actions: {
+      someAction: ()=>console.log('hello')
+    },
+    guards: {
+      someCondition: ()=>true,
+    }
+  });
   const active = current.matches("active");
   const { count } = current.context;
 
